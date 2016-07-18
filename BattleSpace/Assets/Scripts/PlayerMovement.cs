c3bf7b345fd25fd414d74ac7c56b9c7a59575def
totashiya <monkeypuu@gmail.com>
2016-07-18 00:00:52 -0400

    - Imported assets from the Unity Space Shooter tutorial. - Changed the camera to top-down view with a black background. - Created a Player prefab with basic movement controls.

---
+++ BattleSpace/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 0.05f;
+    public float rotationSpeed = 150f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();	// The ship will move forward at all times.
+        Rotate(); // The player will control the rotation of the ship
+	}
+
+    void Move() {
+        transform.Translate(0, 0, speed);
+    }
+
+    void Rotate() {
+        float rotation = Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+    }
+}
