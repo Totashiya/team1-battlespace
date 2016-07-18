b6fa97a2dc2ade33823a19b985f1c7e77cf51960
Christine Chen <monkeypuu@gmail.com>
2016-07-18 12:31:05 +0800

    Created a new script called BulletLife
    This script causes bullets to disappear instantly when hitting the cube, or after 3 seconds when hitting anything that's not the cube.

---
+++ BattleSpace/Assets/Programs/BulletLife.cs
@@ -0,0 +1,33 @@
+﻿/* This script gives bullets a limited lifetime after colliding with something. */
+
+using UnityEngine;
+
+public class BulletLife : MonoBehaviour {
+
+    public float lifeTime = 3.0f; // set amount of time for the countdown
+
+    bool timer = false; // when set to true, a countdown timer will start.
+
+    void Update() {
+        if(timer) {
+            lifeTime -= Time.deltaTime;
+            if(lifeTime <= 0.0f) {
+                SelfDestruct();
+                timer = false;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if(col.gameObject.tag == "Enemies") {
+            SelfDestruct();
+        }
+        else {
+            timer = true;
+        }
+    }
+
+    void SelfDestruct() {
+        Destroy(this.gameObject);
+    }
+}
