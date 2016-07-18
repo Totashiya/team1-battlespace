28a23c34de3e1ccac52c327c7541988c66ab854d
Jason32A <jacksondoe1@hotmail.com>
2016-07-17 20:58:01 -0400

    All the work I've done till now
    Still need to make the enemy act intelligently and shoot back plus GUI
    and all that

---
+++ BattleSpace/Assets/Hits.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hits : MonoBehaviour {
+
+	private int hits = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter (){
+		print ("Hit!");
+		hits += 1;
+		print (hits/4);
+		if (hits % 100 == 0) {
+			Debug.ClearDeveloperConsole ();
+		}
+	}
+}
