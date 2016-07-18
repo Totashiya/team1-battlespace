5f96514a9f663290d7f796ebe3a8bac552c67bcd
Christine Chen <monkeypuu@gmail.com>
2016-07-18 09:00:58 -0400

    + Added CameraFollow.cs: Camera now follows the player + Added Game manager object and GameManager.cs: currently all it does is lock and hide the cursor + Added some temporary rocks to help perceive depth - Changed PlayerMovement.cs: Redid player controls - Changes to Main Camera: Orthographic to Perspective; game is now in third-person

--- BattleSpace/Assets/Scripts/PlayerMovement.cs
+++ BattleSpace/Assets/Scripts/PlayerMovement.cs
@@ -2,26 +2,55 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-    public float speed = 0.05f;
-    public float rotationSpeed = 150f;
+    public float horizontalSpeed = 5f; // speed of ship moving left and right
+	public float rotateSpeed = 20f;
+	public float forwardSpeed = 10f;
+	public float backwardSpeed = -8f;
+	public float decelerationAmount = 0.9f; // bigger value increases deceleration speed
 
-	// Use this for initialization
+	Rigidbody playerRigidbody;
+
+	Vector3 movement;
+
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Move();	// The ship will move forward at all times.
-        Rotate(); // The player will control the rotation of the ship
+		playerRigidbody = GetComponent<Rigidbody> ();
 	}
 
-    void Move() {
-        transform.Translate(0, 0, speed);
+	void FixedUpdate () {
+		float horizontal = Input.GetAxisRaw("Horizontal"); 
+		float vertical = Input.GetAxisRaw ("Vertical");
+
+		Move(horizontal, vertical);	// WASD to move
+        Aim(); // Mouse to aim and rotate the ship
+	}
+
+	void Move(float h, float v) {
+		movement.Set (h, 0, 0);
+		movement *= horizontalSpeed * Time.deltaTime;
+
+		if (h == 1) {
+			playerRigidbody.rotation = Quaternion.Lerp(playerRigidbody.rotation, Quaternion.Euler (0, 0, -20), rotateSpeed * Time.deltaTime);
+		}
+		else if (h == -1) {
+			playerRigidbody.rotation = Quaternion.Lerp(playerRigidbody.rotation, Quaternion.Euler (0, 0, 20), rotateSpeed * Time.deltaTime);
+		}
+		else {
+			playerRigidbody.rotation = Quaternion.Lerp (playerRigidbody.rotation, Quaternion.identity, rotateSpeed * Time.deltaTime);
+		}
+		
+		playerRigidbody.MovePosition (transform.position + movement);
+
+		if(v == 1) {
+			// If W or up is pressed, move forward
+			playerRigidbody.AddForce (transform.forward * forwardSpeed);
+		}
+		else if (v == -1) {
+			// If S or down is pressed, move back
+			playerRigidbody.AddForce (transform.forward * backwardSpeed);
+		}
     }
 
-    void Rotate() {
-        float rotation = Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime;
-        transform.Rotate(0, rotation, 0);
+    void Aim() {
+		// player turns the ship and aims using the mouse
     }
 }
