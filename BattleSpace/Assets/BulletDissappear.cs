ddf71b14636b31e533cd7dd2838eb148f94bd9e2
Jason32A <jacksondoe1@hotmail.com>
2016-07-20 21:17:34 -0400

    More Updates 7/20/16
    7/20/16 update adds bullet disappearing, removes walls, changes control
    scheme, changes camera, and modifies bullets

---
+++ BattleSpace/Assets/BulletDissappear.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletDissappear : MonoBehaviour {
+
+	public float waittime;
+	public GameObject m_Rigidbody;
+
+	private bool DestroyFlag;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SelfDestruct ());
+		DestroyFlag = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (DestroyFlag == true) {
+			Destroy (m_Rigidbody);
+		}
+	}
+
+	IEnumerator SelfDestruct(){
+		yield return new WaitForSeconds (waittime);
+	}
+	void OnTriggerEnter(){
+		DestroyFlag = true;
+	}
+}
