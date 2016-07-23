5036c6023baa80bfa6606956b9bf2cebd7c4415e
Jason32A <jacksondoe1@hotmail.com>
2016-07-22 21:21:56 -0400

    7/22/16 Jason 2121

---
+++ BattleSpace/Assets/Programs/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public int m_SpawnRate;
+	public int m_InitialSpawn;
+	public Transform OriginalSpawn;
+	public Rigidbody Enemy;
+	/*
+	public Transform PlayerTransform;
+	public int scale;
+	public int starter;
+	public GameObject EnemyZero;
+	public GameObject EnemyOne;
+	public GameObject EnemyTwo;
+	public GameObject EnemyThree;
+
+	private int en;
+	private int eu;
+	private int ed;
+	private int et;
+	private int points;
+*/
+	private float time;
+	// Use this for initialization
+	void Start () {
+/*		en = 0;
+		eu = 0;
+		ed = 0;
+		et = 0;
+		points = 0;
+
+*/
+		for (int i = 0; i < m_InitialSpawn; i++){
+			CreateEnemy(i-m_InitialSpawn/2);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (time % m_SpawnRate == 0) {
+			CreateEnemy ();
+		}
+		time += Time.deltaTime;
+	}
+	private void CreateEnemy(float horizontaloffset = 0f){
+		Vector3 Compensation = new Vector3 (horizontaloffset*5, 0f, 0f);
+		Vector3 CreatedEnemy = OriginalSpawn.position + Compensation;
+		GameObject CreatEnemy = Instantiate(Enemy,CreatedEnemy, OriginalSpawn.rotation) as GameObject;
+	}
+}
---
+++ BattleSpace/Assets/Programs/Hits.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hits : MonoBehaviour {
+
+	public GameObject self;
+	public Rigidbody selfr;
+	public int firerate;
+	public Rigidbody m_Shell;
+	public Transform m_FireTransform;
+	public int m_CurrentLaunchForce;
+	public int EnemySpeed;
+
+	private float time;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Adjust the position of the tank based on the player's input.
+
+
+		// Adjust the position of the tank based on the player's input.
+		Vector3 hmovement = transform.right * Random.Range(-1,1) *EnemySpeed *Time.deltaTime;
+
+
+		// Apply this movement to the rigidbody's position
+		selfr.MovePosition (selfr.position + hmovement);
+		if (time % firerate < 0.1) {
+			Fire ();
+		}
+		time += Time.deltaTime;
+
+	}
+
+	void OnTriggerEnter (){
+		Destroy (self);
+		}
+	private void Fire(){
+	// Instantiate and launch the shell.
+		Rigidbody shellInstance = Instantiate (m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+		shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.up;;
+
+	}
+}
